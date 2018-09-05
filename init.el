ec196986a909a149e5ec975ec406eb17b0937366
Rob Agnese <rob@hubindustrial.com>
2018-09-05 08:23:42 -0400

    Tweak garbage collector on boot and otherwise

--- init.el
+++ init.el
@@ -1,6 +1,16 @@
+;; -*- lexical-binding: t -*-
+
 ;;; package --- Emacs init.el
 ;;; Commentary:
 ;;; Code:
+(setq debug-on-error t)
+
+;; Tweak garbage collection
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 ;; No blinking cursor
 (blink-cursor-mode 0)
