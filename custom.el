a0585b24259e7c43183c98b8415a445149df02c7
Rob Agnese <rob@hubindustrial.com>
2018-12-06 09:56:21 -0500

    Remove some junk from custom.el

--- custom.el
+++ custom.el
@@ -3,32 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(compilation-message-face (quote default))
- '(custom-safe-themes
-   (quote
-    ("b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" default)))
- '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
- '(highlight-tail-colors
-   (quote
-    (("#3C3D37" . 0)
-     ("#679A01" . 20)
-     ("#4BBEAE" . 30)
-     ("#1DB4D0" . 50)
-     ("#9A8F21" . 60)
-     ("#A75B00" . 70)
-     ("#F309DF" . 85)
-     ("#3C3D37" . 100))))
- '(lsp-php-language-server-command
-   (quote
-    ("php" "~/.local/opt/php-language-server/vendor/bin/php-language-server.php")))
- '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (docker dockerfile-mode dtrt-indent mocha tide js2-mode nodejs-repl slime company-sourcekit flycheck-swift swift-mode phpcbf company-lsp lsp-ui lsp-php company-php php-mode evil-collection counsel evil-smartparens smartparens rainbow-delimiters cider clojure-mode doom-themes treemacs-evil treemacs-projectile treemacs alchemist elixir-mode elpy auctex which-key use-package toml-mode racer neotree monokai-theme lsp-rust general geiser flycheck-rust exec-path-from-shell evil-search-highlight-persist evil-magit counsel-projectile company column-enforce-mode cargo)))
- '(pos-tip-background-color "#FFFACE")
- '(pos-tip-foreground-color "#1B1D1E")
- '(weechat-color-list
-   (unspecified "#1B1D1E" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
+    (docker dockerfile-mode dtrt-indent mocha tide js2-mode nodejs-repl slime company-sourcekit flycheck-swift swift-mode phpcbf company-lsp lsp-ui lsp-php company-php php-mode evil-collection counsel evil-smartparens smartparens rainbow-delimiters cider clojure-mode doom-themes treemacs-evil treemacs-projectile treemacs alchemist elixir-mode elpy auctex which-key use-package toml-mode racer neotree monokai-theme lsp-rust general geiser flycheck-rust exec-path-from-shell evil-search-highlight-persist evil-magit counsel-projectile company column-enforce-mode cargo))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
