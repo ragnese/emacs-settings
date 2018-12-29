7b52b8343d4466a98490e2265b6124ace26e56aa
Rob Agnese <ragnese@ufl.edu>
2018-12-29 08:34:05 -0500

    Custom.el commit

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (docker dockerfile-mode dtrt-indent mocha tide js2-mode nodejs-repl slime company-sourcekit flycheck-swift swift-mode phpcbf company-lsp lsp-ui lsp-php company-php php-mode evil-collection counsel evil-smartparens smartparens rainbow-delimiters cider clojure-mode doom-themes treemacs-evil treemacs-projectile treemacs alchemist elixir-mode elpy auctex which-key use-package toml-mode racer neotree monokai-theme lsp-rust general geiser flycheck-rust exec-path-from-shell evil-search-highlight-persist evil-magit counsel-projectile company column-enforce-mode cargo))))
+    (flycheck-rust lsp-rust docker dockerfile-mode dtrt-indent mocha tide js2-mode nodejs-repl slime company-sourcekit flycheck-swift swift-mode phpcbf company-lsp lsp-ui lsp-php php-mode evil-collection counsel evil-smartparens smartparens rainbow-delimiters cider clojure-mode treemacs-evil treemacs-projectile treemacs alchemist elixir-mode elpy auctex which-key use-package toml-mode monokai-theme general geiser exec-path-from-shell evil-search-highlight-persist evil-magit counsel-projectile company column-enforce-mode cargo))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
