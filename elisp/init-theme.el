f570ba6787a8acc3bab9d1a9aa8f4fe57598d650
Rob Agnese <rob@hubindustrial.com>
2019-03-29 21:18:44 -0400

    Switch theme to doom-molokai. Maybe faster?

--- elisp/init-theme.el
+++ elisp/init-theme.el
@@ -4,11 +4,17 @@
 
 ;; Color scheme
 (use-package monokai-theme
+  :disabled
   :ensure t
   :config
   (setq monokai-background "#161A1F")
   (setq monokai-comments "#8C8C8C")
   (load-theme 'monokai t))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-molokai t))
 
 (provide 'init-theme)
 ;;; init-theme ends here
