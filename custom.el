0fce3bf9e3b85f0d9fad02a896c953a062832771
Rob Agnese <ragnese@protonmail.com>
2019-05-22 20:06:42 -0400

    update custom.el

--- custom.el
+++ custom.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-collection-company-use-tng nil t)
  '(package-selected-packages
    (quote
-    (request evil-smartparens evil-surround doom-themes projectile-ripgrep nyan-mode ibuffer-projectile ox-hugo auctex ibuffer-vc lsp-ui lsp-php lsp-rust docker dockerfile-mode mocha tide js2-mode nodejs-repl slime flycheck-swift swift-mode phpcbf company-lsp php-mode evil-collection counsel smartparens rainbow-delimiters cider clojure-mode treemacs-evil treemacs-projectile treemacs alchemist elixir-mode elpy which-key use-package toml-mode monokai-theme general geiser exec-path-from-shell evil-search-highlight-persist evil-magit counsel-projectile company cargo))))
+    (evil-smartparens evil-surround projectile-ripgrep nyan-mode ibuffer-projectile ox-hugo auctex ibuffer-vc lsp-ui lsp-php lsp-rust docker dockerfile-mode mocha tide js2-mode nodejs-repl slime flycheck-swift swift-mode company-lsp php-mode evil-collection counsel smartparens rainbow-delimiters cider clojure-mode treemacs-evil treemacs-projectile treemacs alchemist elixir-mode elpy which-key use-package toml-mode monokai-theme general geiser exec-path-from-shell evil-search-highlight-persist evil-magit counsel-projectile company cargo))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
